b26cb7f7beea48d1065c688b84ab65847cbd97bd
carlos avendaño <carlosavendano43@gmail.com>
2022-09-29 11:27:00 -0300

    add log

--- helpers/Htmlpdf.helpers.js
+++ helpers/Htmlpdf.helpers.js
@@ -3,12 +3,14 @@ const pdf = require('html-pdf');
 const axios = require("axios");
 const { logger } = require('../config/pino');
 exports.generarPDF = async(html,name)=>{
+    
     try{
         logger.info(`[generarPDF] inicio`);
         const file=`../frontend/src/assets/storage/finiquito/${name}`;
         const url = process.env.API_PORTAL_FINIQUITO;
         logger.info(`[generarPDF] url ${url}`);
-        
+        logger.info(`[generarPDF] html: ${html}`);
+        logger.info(`[generarPDF] name: ${name}`)
         let response;
         if(process.env.AMBIENT === 'QA'){
             response = pdf.create(html).toFile(file, function(err, resp) {
@@ -23,7 +25,7 @@ exports.generarPDF = async(html,name)=>{
         } else {
             response = await pdf.create(html).toBuffer(async function(err, buffer){
                 try{
-                    logger.info(`[generarPDF] buffer: ${buffer}`);
+                    
                     const string64 = buffer.toString('base64');
                     const resp =  await axios.post(url, {
                         buffer:string64,
@@ -32,6 +34,7 @@ exports.generarPDF = async(html,name)=>{
                     logger.info(`[generarPDF] response: ${JSON.stringify(resp)}`);
                     return resp.data;
                 } catch(e) {
+                    logger.info(`[generarPDF] error: ${JSON.stringify(e)}`);
                     return e;
                 }
                 //return FileUploadS3(buffer,name);
