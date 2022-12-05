1d1bae588d9ccca250495e3ce06d9db4230cdbf3
Carlos Avendaño <carlosavendano43@gmail.com>
2022-12-05 00:51:58 -0300

    change librery pdf

--- config/database.js
+++ config/database.js
@@ -1,11 +1,7 @@
 const mongoose = require('mongoose');
 const { logger } = require('./pino');
 const connectDB = async()=>{
-   await mongoose.connect(process.env.DB_URL,{
-    useNewUrlParser:true,
-    useFindAndModify:false,
-    useUnifiedTopology: true,
-    }).then(db => logger.info('Database esta conectada 1')
+   await mongoose.connect(process.env.DB_URL).then(db => logger.info('Database esta conectada 1')
     ).catch(err => console.log(err));
 }
 
--- helpers/Htmlpdf.helpers.js
+++ helpers/Htmlpdf.helpers.js
@@ -1,4 +1,6 @@
 const pdf = require('html-pdf');
+const pdf_html = require('html-pdf-node');
+const options = { format: 'A4' };
 //const {FileUploadS3} = require('./AWS-Helper.js');
 const axios = require("axios");
 const { logger } = require('../config/pino');
@@ -23,7 +25,25 @@ exports.generarPDF = async(html,name,res)=>{
                 }
             });
         } else {
-            response = await pdf.create(html).toBuffer(async (err, buffer)=>{
+            const file = { content: html };
+            response = pdf_html.generatePdf(file, options).then(async buffer => {
+
+                try{
+                    const string64 = buffer.toString('base64');
+                    const resp =  await axios.post(url, {
+                        buffer:string64,
+                        name:name,
+                    });
+                    
+                    logger.info(`[generarPDF] response portaldesoluciones: ${JSON.stringify(resp.data)}`);
+                    const responsep = await res.status(200).json(resp.data);
+                    return responsep;
+                } catch(e) {
+                    logger.info(`[generarPDF] error portaldesoluciones: ${JSON.stringify(e)}`);
+                    return res.status(404).json({"error":e});
+                }
+            });
+            /*response = await pdf.create(html).toBuffer(async (err, buffer)=>{
                 try{
                     const string64 = buffer.toString('base64');
                     const resp =  await axios.post(url, {
@@ -39,8 +59,9 @@ exports.generarPDF = async(html,name,res)=>{
                     return res.status(404).json({"error":e});
                 }
                 //return FileUploadS3(buffer,name);
-            });
+            });*/
         }
+        console.log(response);
      
     } catch(e) {
         console.log(e);
