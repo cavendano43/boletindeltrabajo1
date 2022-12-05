f4bda5682f9ef1cfaa7f4e52270f3b16368617d7
Carlos Avendaño <carlosavendano43@gmail.com>
2022-12-04 22:23:22 -0300

    delete phantom

--- helpers/Htmlpdf.helpers.js
+++ helpers/Htmlpdf.helpers.js
@@ -2,9 +2,7 @@ const pdf = require('html-pdf');
 //const {FileUploadS3} = require('./AWS-Helper.js');
 const axios = require("axios");
 const { logger } = require('../config/pino');
-const options={
-    "phantomPath": "./node_modules/phantomjs/bin/phantomjs", 
-}
+
 exports.generarPDF = async(html,name,res)=>{
     
     try{
